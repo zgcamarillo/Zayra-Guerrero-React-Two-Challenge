1ba28fed684f88528f687e8fba4f678b2996a3c5
Zayra Guerrero Camarillo <zayraadielencamarillo@gmail.com>
2026-02-06 13:42:20 -0500

    Completed To Do List

--- todo-router-app/src/App.jsx
+++ todo-router-app/src/App.jsx
@@ -1,33 +1,78 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route, Link, useParams, useNavigate } from "react-router-dom"
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function TodoDetail({ todos, deleteTodo }) {
+  const {id } = useParams()
+  const todo = todos.find((t) => String(t.id) === id)
+  const navigate = useNavigate()
+
+  if (!todo) return <p>To Do not found</p>
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h2>To Do Detail</h2>
+    <p>{todo.text}</p>
+    <button 
+      onClick={() => {
+      deleteTodo(todo.id) 
+      navigate("/")}} >
+      Delete
+      </button>
+    <Link to="/">Back</Link>
+    </>  
+  )
+}
+
+
+function App() {
+
+  const [todos, setTodos] = useState([])
+  const [newTodo, setNewTodo] =useState("")
+
+  function addTodo() {
+    if(!newTodo.trim()) return 
+
+    setTodos([...todos, { id: Date.now(), text: newTodo}])
+    setNewTodo("")
+  }
+
+  function deleteTodo(id) {
+  setTodos(todos.filter((todo) => todo.id !== id))
+}
+
+  return (
+    <>
+      <h1>To Do</h1>
+
+      <Routes>
+
+        <Route 
+          path="/" 
+          element={
+            <>
+              <input
+                value={newTodo}
+                onChange={(e) => setNewTodo(e.target.value)}
+                placeholder="New To Do"
+              />
+
+              <button onClick={addTodo}>Add To Do</button>
+
+              <ul>
+                {todos.map((todo) => (
+                  <li key={todo.id}>
+                    <Link to={`/todo/${todo.id}`}>{todo.text}</Link>
+                  </li>
+                ))}
+              </ul>
+
+            </>
+          } 
+        />
+
+        <Route path="/todo/:id" element={<TodoDetail todos={todos} deleteTodo={deleteTodo} />} />
+      </Routes>
     </>
   )
 }
