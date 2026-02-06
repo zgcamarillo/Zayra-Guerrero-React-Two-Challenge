92cb118c3d480d01476e41073b673daf1719b90f
Zayra Guerrero Camarillo <zayraadielencamarillo@gmail.com>
2026-02-06 12:03:34 -0500

    Complete Movie Search App

--- movie-search-app/src/App.jsx
+++ movie-search-app/src/App.jsx
@@ -1,34 +1,51 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState([]);
 
+  async function handleSearch() {
+  const apiKey = import.meta.env.VITE_TMDB_API_KEY
+
+  const response = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`
+  )
+
+  const data = await response.json()
+  setMovies(data.results)
+
+  console.log(data.results);
+}
+
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <h1>Movie Search</h1>
+
+      <input 
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search for a movie..."
+        />
+        <button onClick={handleSearch}>Search</button>
+        {movies.length === 0 && <p>No results yet</p>}
+        
+        <h2>Results</h2>
+        <ul>
+          {movies.map((movie) => (
+            <li key={movie.id}>
+              <p>{movie.title}</p>
+              {movie.poster_path && (
+                <img
+                  src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                  alt={movie.title}
+                />
+              )}
+             </li>
+          ))}
+        </ul>
+      </>
   )
 }
 
