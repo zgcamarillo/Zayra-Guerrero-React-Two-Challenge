afdabd8766cb72ea249409469996e625143f1b0b
Zayra Guerrero Camarillo <zayraadielencamarillo@gmail.com>
2026-02-07 11:30:10 -0500

    Added background styling for Counter App

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -5,17 +5,25 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div className="card">
-        <h1>{count}</h1>
-        <button onClick={() => setCount((count) => count + 1)}>
-          Increase
-        </button>  
-        <button onClick={() => setCount((count) => count -1)}>
-          Decrease
-        </button>
+    <div className="page">
+      <div className="cloud">
+        <div className="puffs">
+          
+        </div>
       </div>
-    </>
+      <h1>Countly</h1>
+      <div className="card">
+        <h3>{count}</h3>
+          <div className="btn-row">
+            <button id="increase" onClick={() => setCount((count) => count + 1)}>
+              Increase
+            </button>  
+            <button id="decrease" onClick={() => setCount((count) => count -1)}>
+              Decrease
+            </button>
+          </div>
+      </div>
+    </div>
   )
 }
 
