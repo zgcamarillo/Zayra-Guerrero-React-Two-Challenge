c1b028bc99fabd744f67419875e3a6d1699e60f1
Zayra Guerrero Camarillo <zayraadielencamarillo@gmail.com>
2026-02-08 10:46:11 -0500

    Counter-App fixed visibility, finished styling

--- counter-app/src/main.jsx
+++ counter-app/src/main.jsx
@@ -1,8 +1,9 @@
+import ReactDOM from 'react-dom/client'
 import { StrictMode } from 'react'
 import './index.css'
 import App from './App.jsx'
 
-ReactDom.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
